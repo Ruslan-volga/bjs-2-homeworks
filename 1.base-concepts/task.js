b2723e83b31c53fe4be667ec99e8f72b0204de22
Ruslan-Volga <rustikty@gmail.com>
2025-02-23 21:00:42 +0300

    обратные кавычки + result

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -16,8 +16,8 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let percentageRate = parseFloat( percent / 100 / 12 );
   let loanBody =amount - contribution;
   let monthlyPayment = loanBody * (percentageRate + (percentageRate / ((1 + percentageRate)**countMonths - 1)));
-  let fullCount = parseFloat(monthlyPayment * countMonths);
-return fullCount.toFixed(2);
+  let fullCount = monthlyPayment * countMonths;
+  let resul = tfullCount.toFixed(2);
 return `\'${result}\'`
 }
 
