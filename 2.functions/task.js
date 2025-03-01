5748a17673af8218010ef116b098cb6b41ac963b
Ruslan-Volga <rustikty@gmail.com>
2025-03-02 00:05:45 +0300

    Решение задач. Функции. Исправлена разница. const=arr

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -60,12 +60,13 @@ function averageEvenElementsWorker(...arr) {
   let sumEvenElement = 0; 
   let countEvenElement = 0; 
   for (let i = 0; i < arr.length; ++i) {
-     if (arr.length === 0) {
-        return 0;
-      } else (0== arr[i] % 2)  { 
-        sumEvenElement += arr[i]; 
-        countEvenElement += 1; 
+    if (arr.length === 0) {
+      return 0;
     }
+    if(0== arr[i] % 2)  { 
+          sumEvenElement += arr[i]; 
+          countEvenElement += 1; 
+      } 
   } 
   return  countEvenElement ? sumEvenElement / countEvenElement : NaN;
 }
