a07695700c105e2a4a876541badccbede8371219
Ruslan-Volga <rustikty@gmail.com>
2025-03-02 03:25:52 +0300

    initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,5 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every((el, idx) => arr1.length === arr2.length && el === arr2[idx]); 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
