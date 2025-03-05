7540403c7a4a029ac0bb22cf068b63062b953af4
Ruslan-Volga <rustikty@gmail.com>
2025-03-05 22:20:52 +0300

    final decision-1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -8,7 +8,7 @@ function getUsersNamesInAgeRange(users, gender) {
 
 function getUsersNamesInAgeRange(people, gender) {
   
-    let mas = users.filter(item => item.gender === gender).map(item => item.age);
+    let mas = people.filter(item => item.gender === gender).map(item => item.age);
     let res = mas.reduce((sum, age) => sum + age, 0);
     
     return (mas.length > 0) ? res / mas.length : 0;
