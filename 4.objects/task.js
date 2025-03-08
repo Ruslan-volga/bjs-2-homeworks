9dd22ac0b65af5eadff93953232735bc5d06a862
Ruslan-Volga <rustikty@gmail.com>
2025-03-08 18:13:28 +0300

    final decision one more

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -40,6 +40,6 @@ console.log(student1);
 // {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
 let student2 = new Student("Артём", "мужской", 25);
 student2.setSubject("Geometry");
-student2.exclude('плохая учёба');
+student2.exclude('прогулы');
 console.log(student2);
 // {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
