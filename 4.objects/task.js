9aa972593f5779737246c9872fea50f4afd6939b
Ruslan-Volga <rustikty@gmail.com>
2025-03-08 20:02:41 +0300

    finaly decision

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -13,30 +13,28 @@ Student.prototype.addMarks = function(...marks) {
     if (!this.arrMarks) {
         return; 
     }
-    this.arrMarks.push(...marks); 
+    this.arrMarks.push(...marks);
 }
 
 Student.prototype.getAverage = function() {
     if (!this.arrMarks || this.arrMarks.length === 0) {
         return 0; 
     }
-    const sum = this.arrMarks.reduce((acc, mark) => acc + mark, 0); 
-    return sum / this.arrMarks.length; 
+    const sum = this.arrMarks.reduce((acc, mark) => acc + mark, 0);
 }
-
 Student.prototype.exclude = function(reason) {
-    delete this.subject;
+    delete this.subject; 
     delete this.arrMarks; 
     this.excluded = reason; 
+
 }
 
-// Пример использования
 let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Алгебра");
 console.log(student1.getAverage()); // 0
 student1.addMarks(4, 5, 4, 5);
 console.log(student1.getAverage()); // 4.5
-console.log(student1); // {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Алгебра"}
+console.log(student1); // {age: 19, gender: "женский", Marks: [4, 5, 4, 5], name: "Василиса", subject: "Алгебра"}
 
 let student2 = new Student("Артем", "мужской", 25);
 student2.setSubject("Геометрия");
