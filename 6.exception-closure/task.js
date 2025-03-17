f41760c0ee667df2642a87bc198d382b3e10f0d3
Ruslan-Volga <rustikty@gmail.com>
2025-03-17 21:49:06 +0300

    finaly decision-6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,61 +1,4 @@
-﻿/*function parseCount(numberOfUnits){
-    let parseNumber = Number.parseFloat(numberOfUnits);
-    if (isNaN(parseNumber)){
-        throw new Error("«Невалидное значение»");
-        }
-    return parseNumber;
-    }
-
-    function validateCount(numberOfUnits) {
-       try {
-        return parseCount(numberOfUnits);
-       } catch (error) {
-            return error;
-       }
-       
-    }
-
-
-
-    class Triangle{
-        constructor(a, b, c){
-            if ((a + b) < c && (a + c) < b && (b + c) < a)
-                {
-                    throw new Error("Треугольник с такими сторонами не существует");
-                }
-                this.a = a;
-                this.b = b;
-                this.c = c;
-            }
-        
-
-        get perimeter(){
-            return this.a + this.b + this.c;
-        }
-
-        get area(){
-          let p = this.perimeter / 2;
-          return Number(Math.sqrt(p*(p - this.a)*(p - this.b)*(p - this.c)).toFixed(3));
-        }
-    }
-
-    function getTriangle(a, b, c) {
-        try{
-            return new Triangle(a, b, c);
-        }
-        catch(error){
-            return{
-                get perimeter(){
-                    return "Ошибка! Треугольник не существует";
-                },
-                get area(){
-                    return "Ошибка! Треугольник не существует";
-                }
-            }
-        }
-    }
-*/
-
+﻿
 
     function parseCount(value) {
         const result = Number.parseFloat(value);
@@ -75,7 +18,7 @@
     
     class Triangle {
         constructor(a, b, c) {
-            if (a + b <= c || a + c <= b || b + c <= a) {
+            if (a + b < c || a + c < b || b + c < a) {
                 throw new Error("Треугольник с такими сторонами не существует");
             }
             this.a = a;
@@ -88,8 +31,8 @@
         }
     
         get area() {
-            const s = this.perimeter / 2;
-            return +Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(3);
+            const p = this.perimeter / 2;
+            return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
         }
     }
     
