418bfe134f9d988e7d676a43439fd37195276530
Ruslan-Volga <rustikty@gmail.com>
2025-03-17 21:40:15 +0300

    finaly decision-6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,4 +1,4 @@
-﻿function parseCount(numberOfUnits){
+﻿/*function parseCount(numberOfUnits){
     let parseNumber = Number.parseFloat(numberOfUnits);
     if (isNaN(parseNumber)){
         throw new Error("«Невалидное значение»");
@@ -52,5 +52,58 @@
                     return "Ошибка! Треугольник не существует";
                 }
             }
+        }
+    }
+*/
+
+
+    function parseCount(value) {
+        const result = Number.parseFloat(value);
+        if (isNaN(result)) {
+            throw new Error("Невалидное значение");
+        }
+        return result;
+    }
+    
+    function validateCount(value) {
+        try {
+            return parseCount(value);
+        } catch (error) {
+            return error;
+        }
+    }
+    
+    class Triangle {
+        constructor(a, b, c) {
+            if (a + b <= c || a + c <= b || b + c <= a) {
+                throw new Error("Треугольник с такими сторонами не существует");
+            }
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    
+        get perimeter() {
+            return this.a + this.b + this.c;
+        }
+    
+        get area() {
+            const s = this.perimeter / 2;
+            return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(3);
+        }
+    }
+    
+    function getTriangle(a, b, c) {
+        try {
+            return new Triangle(a, b, c);
+        } catch (error) {
+            return {
+                get area() {
+                    return "Ошибка! Треугольник не существует";
+                },
+                get perimeter() {
+                    return "Ошибка! Треугольник не существует";
+                }
+            };
         }
     }
